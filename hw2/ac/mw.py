3d87b3328edb9060195ea962b30a223d2e67a4b3
doug chang <dougc@stanfordalumni.org>
2026-03-28 10:13:37 -0700

    fix reward

--- hw2/ac/mw.py
+++ hw2/ac/mw.py
@@ -99,8 +99,7 @@ class MetaWorldEnv:
         # Debug: print raw env info occasionally
         if self._step is not None and self._step < 100:
             print("debug info:", info)
-
-        reward = np.float32(1.0 * info.get("success", 0.0))
+        #reward = np.float32(1.0 * info.get("success", 0.0))
         self._step += 1
         if self._step >= self.duration:
             done = True
